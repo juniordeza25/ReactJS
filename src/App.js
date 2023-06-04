cd3e7a4312b587aef4b30136914774b162eff255
juniordeza25 <juniordezaramos@gmail.com>
2023-06-04 19:26:58 -0400

    Primer Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+/*Se importa el componente y se le asigna un nombre para ser usado, dicho
+nombre es recomendable que sea igual que el componente para que sea facil ubicarlo
+mas adelante en el codigo*/
+import HolaMundo from './Components/HolaMundo.js';
+
+/*Para usar el componente que no es por default se debe hacer mediante llaves
+y sigue la misma regla del nombre explicado anteriormente*/
+import { AdiosMundo } from './Components/HolaMundo.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*Para usar el compoente importado se usa el nombre que le damos en el 
+        import como una etiqueta HTML*/}
+        <HolaMundo/>
+        <AdiosMundo/>
         <a
           className="App-link"
           href="https://reactjs.org"
