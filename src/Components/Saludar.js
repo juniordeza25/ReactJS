4a712d7916aba2cb57122d13799c7f3f72820c69
juniordeza25 <juniordezaramos@gmail.com>
2023-06-05 13:28:34 -0400

    Implementando la asignacion por destructuring

--- src/Components/Saludar.js
+++ src/Components/Saludar.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export default function Saludar(props) {
   console.log(props);
+  /* La asignacion por destructuring sirve para sacar los valor recibidos en los props y asi
+  al requerir una prop se usará el nombre asignado y no será necesario hacer una escala para
+  llegar al prop que se quiere usar */
+  const { userInfo, saludarFn } = props;
+  const { nombre } = userInfo;
 
   return (
     <div>
@@ -9,15 +14,13 @@ export default function Saludar(props) {
         Hola {props.name} tiene {props.edad} años.
       </h2> */}
       <p>
-        Pasando usuario como objeto: {props.userInfo.nombre} tiene{" "}
-        {props.userInfo.edad}, color fav {props.userInfo.color}
+        Pasando usuario como objeto: {nombre} tiene {props.userInfo.edad}, color
+        fav {props.userInfo.color}
       </p>
       {/* para usar una funcion en el evento onclick recibiendo un valor como parametro se
       debe hacer con la llamada "funcion anónima" solo asi esta funcion se ejecutará cuando
       se haga click en el boton, de lo contrario al recibir una funcion esta se ejecutará sola */}
-      <button onClick={() => props.saludarFn(props.userInfo.nombre)}>
-        Saludar
-      </button>
+      <button onClick={() => saludarFn(nombre)}>Saludar</button>
     </div>
   );
 }
